426899d3218ee44e866799f117a8b9bc08b07c69
Sean Watkins <slwatkins@uh.edu>
2021-09-01 09:06:45 -0500

    Update to using config.serve_static_files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ if Iqvoc::Cedar.const_defined?(:Application)
   Iqvoc::Cedar::Application.configure do
     # Settings specified here will take precedence over those in config/environment.rb
     Iqvoc::Environments.setup_production(config)
-    config.serve_static_assets = true
+    config.serve_static_files = true
     if ENV["RAILS_LOG_TO_STDOUT"].present?
       logger           = ActiveSupport::Logger.new(STDOUT)
       logger.formatter = config.log_formatter
