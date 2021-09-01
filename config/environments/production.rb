200b587308f98670a374c7ef41c68d9328274a66
Sean Watkins <slwatkins@uh.edu>
2021-08-31 20:10:48 -0500

    Add RAILS_LOG_TO_STDOUT to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,11 @@ if Iqvoc::Cedar.const_defined?(:Application)
     # Settings specified here will take precedence over those in config/environment.rb
     Iqvoc::Environments.setup_production(config)
     config.serve_static_assets = true
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
     config.log_level = :warn
   end
 end
