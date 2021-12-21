1c65152358eefbd0b5729f62155fed357885548a
Sean Watkins <slwatkins@uh.edu>
2021-12-21 12:23:36 -0600

    Enable caching for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ if Iqvoc::Cedar.const_defined?(:Application)
   Iqvoc::Cedar::Application.configure do
     # Settings specified here will take precedence over those in config/environment.rb
     Iqvoc::Environments.setup_production(config)
+
+    config.action_controller.perform_caching = true
+    config.cache_store = :memory_store, { size: 64.megabytes }
+
     config.serve_static_files = true
     if ENV["RAILS_LOG_TO_STDOUT"].present?
       logger           = ActiveSupport::Logger.new(STDOUT)
